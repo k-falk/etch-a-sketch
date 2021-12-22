a4c803e4e5f1af267e102c16a3dd6c197cba2591
k-falk <xkfalkx@gmail.com>
2021-12-21 23:06:56 -0500

    Added buttons to clear and change size. Also cleaned up UI a bit

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 
-function loadSketchBox(){
+function loadSketchBox(inputArea){
 
 const sketchBox = document.querySelector(".sketch-box");
 
-const area = 16;
+const area = inputArea;
 
 for(let i = 0; i < area; i++){
     const row = document.createElement("div");
@@ -15,6 +15,48 @@ for(let i = 0; i < area; i++){
             
         }
         sketchBox.appendChild(row);
+        listenToPixels();
     }
 }
-loadSketchBox();
+loadSketchBox(16);
+
+function listenToPixels(){
+
+    const pixels = document.querySelectorAll(".pixel");
+
+    pixels.forEach((pixel) => {
+
+        pixel.addEventListener("mouseover", function( event ) {
+            // highlight the mouseover target
+            event.target.style.backgroundColor = "black";
+
+        }, false);
+    });
+}
+
+const clearButton = document.querySelector(".clear");
+
+clearButton.addEventListener("click", function( event ) {
+    // highlight the mouseover target
+        const pixels = document.querySelectorAll(".pixel");
+        pixels.forEach((pixel) => { pixel.style.backgroundColor = "white";
+
+    }, false)
+}); 
+
+const sizeButton = document.querySelector(".size-button");
+
+sizeButton.addEventListener("click", function( event ) {
+    // highlight the mouseover target
+    let newSize = Number(prompt("Please enter new size", "32"));
+    deleteNodeContents(document.querySelector(".sketch-box"));
+    loadSketchBox(newSize);
+
+    }, false); 
+
+
+function deleteNodeContents(node){
+    var range = document.createRange();
+    range.selectNodeContents(node);
+    range.deleteContents();
+}
