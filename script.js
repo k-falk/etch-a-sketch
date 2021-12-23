2b4478e72db045ad2f2ab07f37f4b2f12018bdd5
k-falk <xkfalkx@gmail.com>
2021-12-23 00:14:37 -0500

    Fixed clear button

--- script.js
+++ script.js
@@ -22,17 +22,16 @@ for(let i = 0; i < area; i++){
 }
 loadSketchBox(16);
 
+
+//function get_random_color found on stackoverflow by "Blixt"
 function get_random_color() 
 {
-    var color = "";
-    for(var i = 0; i < 3; i++) {
-        var sub = Math.floor(Math.random() * 256).toString(16);
-        color += (sub.length == 1 ? "0" + sub : sub);
-    }
-    return "#" + color;
+    let color = Math.floor(Math.random() * 16777216).toString(16);
+    // Avoid loops.
+    return '#000000'.slice(0, -color.length) + color;
 }
 
-
+//Function RGB_Linear_Shade on stack overflow thanks to "Pimp Trizkit"
 const RGB_Linear_Shade=(p,c)=>{
     var i=parseInt,r=Math.round,[a,b,c,d]=c.split(","),P=p<0,t=P?0:255*p,P=P?1+p:1-p;
     return"rgb"+(d?"a(":"(")+r(i(a[3]=="a"?a.slice(5):a.slice(4))*P+t)+","+r(i(b)*P+t)+","+r(i(c)*P+t)+(d?","+d:")");
@@ -48,13 +47,11 @@ function listenToPixels(){
             
             // highlight the mouseover target
             if(rainbowMode == true){
-                if(event.target.style.backgroundColor === ""){
-                    console.log("Changing color to random color")
+                if(event.target.style.backgroundColor === "" || event.target.style.backgroundColor == "white"){
                     let color = get_random_color();
                     event.target.style.backgroundColor = color;
                     return false;
                 }else{
-                    console.log("Making current color darker")
                     let darkerColor = RGB_Linear_Shade(-.20, event.target.style.backgroundColor);
                     event.target.style.backgroundColor = darkerColor;
                     return false;
@@ -94,10 +91,13 @@ sizeButton.addEventListener("click", function( event ) {
 const rainbowButton = document.querySelector(".rainbow-mode");
 
 rainbowButton.addEventListener("click", function( event ) {
-    if(rainbowMode == false)
+    if(rainbowMode == false){
         rainbowMode = true;
-    else 
+        rainbowButton.classList.add("enabled");
+    }else{
         rainbowMode = false;
+        rainbowButton.classList.remove("enabled")
+    }
 
 
 
